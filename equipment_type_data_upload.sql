4d9733f7d9a0429f184cdabcaf1b29b9d1706e9a
iagarciap <iagarciapretel@email.wm.edu>
2016-10-03 15:58:51 -0400

    Add files via upload

---
+++ equipment_type_data_upload.sql
@@ -0,0 +1,41 @@
+use project1;
+
+insert into equipmenttype (EquipmentName, StudentFee, FacultyFee, AlumniFee, GuestFee) values ('Rock Climbing Kit',0.00, 2.00, 4.00, 6.00);
+
+insert into equipmenttype (EquipmentName, StudentFee, FacultyFee, AlumniFee, GuestFee) values ('Backpack', 0.00, 3.00, 5.00, 7.00);
+
+insert into equipmenttype (EquipmentName, StudentFee, FacultyFee, AlumniFee, GuestFee) values ('Tent', 5.00, 6.00, 7.00, 8.00);
+
+insert into equipmenttype (EquipmentName, StudentFee, FacultyFee, AlumniFee, GuestFee) values ('Cooler', 0.00, 2.00, 3.00, 4.00);
+
+insert into equipmenttype (EquipmentName, StudentFee, FacultyFee, AlumniFee, GuestFee) values ('Bike', 5.00,7.00,8.00,10.00);
+
+insert into equipmenttype (EquipmentName, StudentFee, FacultyFee, AlumniFee, GuestFee) values ('Sleeping Bag', 0.00, 2.00, 4.00, 5.00);
+
+insert into equipmenttype (EquipmentName, StudentFee, FacultyFee, AlumniFee, GuestFee) values ('Corn Hole', 0.00, 2.00,3.00,4.00);
+
+insert into equipmenttype (EquipmentName, StudentFee, FacultyFee, AlumniFee, GuestFee) values ('Cooking Kit', 5.00, 6.00, 6.00, 7.00);
+
+insert into equipmenttype (EquipmentName, StudentFee, FacultyFee, AlumniFee, GuestFee) values ('Sleeping Pad', 0.00, 1.00, 1.00, 2.00);
+
+insert into equipmenttype (EquipmentName, StudentFee, FacultyFee, AlumniFee, GuestFee) values ('Camping Chairs', 0.00,2.00,3.00,4.00);
+
+insert into equipmenttype (EquipmentName, StudentFee, FacultyFee, AlumniFee, GuestFee) values ('Hammock', 2.00,2.00,2.00,3.00);
+
+insert into equipmenttype (EquipmentName, StudentFee, FacultyFee, AlumniFee, GuestFee) values ('Hanging Tent', 10.00,10.00,10.00,10.00);
+
+insert into equipmenttype (EquipmentName, StudentFee, FacultyFee, AlumniFee, GuestFee) values ('Kayak', 5.00, 6.00, 7.00, 8.00);
+
+insert into equipmenttype (EquipmentName, StudentFee, FacultyFee, AlumniFee, GuestFee) values ('Life Vest', 0.00,1.00,2.00,3.00);
+
+insert into equipmenttype (EquipmentName, StudentFee, FacultyFee, AlumniFee, GuestFee) values ('Fishing Rod', 2.00, 3.00, 4.00, 5.00);
+
+insert into equipmenttype (EquipmentName, StudentFee, FacultyFee, AlumniFee, GuestFee) values ('Flag Football Kit', 3.00,4.00,5.00,6.00);
+
+insert into equipmenttype (EquipmentName, StudentFee, FacultyFee, AlumniFee, GuestFee) values ('Capture the Flag Kit', 0.00, 2.00, 2.00, 2.00);
+
+insert into equipmenttype (EquipmentName, StudentFee, FacultyFee, AlumniFee, GuestFee) values ('Field Lining Set', 2.00, 3.00, 3.00, 4.00);
+
+insert into equipmenttype (EquipmentName, StudentFee, FacultyFee, AlumniFee, GuestFee) values ('Beach Volleyball Kit', 3.00,3.00,3.00,4.00);
+
+insert into equipmenttype (EquipmentName, StudentFee, FacultyFee, AlumniFee, GuestFee) values ('First Aid Kit', 0.00, 0.00, 0.00, 0.00);
