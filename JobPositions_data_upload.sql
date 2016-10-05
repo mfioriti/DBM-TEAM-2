1b2713fcf8ead5dfb8ebc5e623719318bb958a11
iagarciap <iagarciapretel@email.wm.edu>
2016-10-05 17:37:20 -0400

    JobPositions table update
    note: I named the database project1 on my computer

---
+++ JobPositions_data_upload.sql
@@ -0,0 +1,41 @@
+use project1;
+
+insert into jobpositions (JobDescription) values ('Fishing Guide');
+
+insert into jobpositions (JobDescription) values ('Hiking Guide');
+
+insert into jobpositions (JobDescription) values ('Rental Staff');
+
+insert into jobpositions (JobDescription) values ('Ropes Course Instructor');
+
+insert into jobpositions (JobDescription) values ('White Water Rafting Guide');
+
+insert into jobpositions (JobDescription) values ('Rock Climbing Instructor');
+
+insert into jobpositions (JobDescription) values ('Manager');
+
+insert into jobpositions (JobDescription) values ('Administrative Staff');
+
+insert into jobpositions (JobDescription) values ('Janitor');
+
+insert into jobpositions (JobDescription) values ('First Aid Instructor');
+
+insert into jobpositions (JobDescription) values ('Equipment Manager');
+
+insert into jobpositions (JobDescription) values ('Student Assistant');
+
+insert into jobpositions (JobDescription) values ('Chief Operating Officer');
+
+insert into jobpositions (JobDescription) values ('Vehicle Operator');
+
+insert into jobpositions (JobDescription) values ('Accountant');
+
+insert into jobpositions (JobDescription) values ('Camping Trip Guide');
+
+insert into jobpositions (JobDescription) values ('Canoe/Kayak Instructor');
+
+insert into jobpositions (JobDescription) values ('Front Desk Staff');
+
+insert into jobpositions (JobDescription) values ('Intern');
+
+insert into jobpositions (JobDescription) values ('Security Guard');
