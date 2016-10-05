4d3bbb1e7367975bc18e2e78644ca3382e78f1ad
iagarciap <iagarciapretel@email.wm.edu>
2016-10-05 18:58:49 -0400

    employment history data

---
+++ employmenthistory_data_upload.sql
@@ -0,0 +1,45 @@
+use project1;
+
+insert into employmenthistory (EmployeeID, PositionID, StartDate, EndDate) values ( 1000, 20, '1993-11-08', Null);
+
+insert into employmenthistory (EmployeeID, PositionID, StartDate, EndDate) values ( 1001, 19, '2000-11-08', '2001-11-08');
+
+insert into employmenthistory (EmployeeID, PositionID, StartDate, EndDate) values ( 1002, 18, '2005-11-08', Null);
+
+insert into employmenthistory (EmployeeID, PositionID, StartDate, EndDate) values ( 1003, 17, '2010-11-08', Null);
+
+insert into employmenthistory (EmployeeID, PositionID, StartDate, EndDate) values ( 1004, 16, '2015-11-08', Null);
+
+insert into employmenthistory (EmployeeID, PositionID, StartDate, EndDate) values ( 1005, 15, '2001-11-08', Null);
+
+insert into employmenthistory (EmployeeID, PositionID, StartDate, EndDate) values ( 1006, 14, '2006-11-08', Null);
+
+insert into employmenthistory (EmployeeID, PositionID, StartDate, EndDate) values ( 1007, 13, '2011-11-08', Null);
+
+insert into employmenthistory (EmployeeID, PositionID, StartDate, EndDate) values ( 1008, 12, '2015-11-08', Null);
+
+insert into employmenthistory (EmployeeID, PositionID, StartDate, EndDate) values ( 1009, 11, '1998-11-08', Null);
+
+insert into employmenthistory (EmployeeID, PositionID, StartDate, EndDate) values ( 1010, 10, '1996-11-08', Null);
+
+insert into employmenthistory (EmployeeID, PositionID, StartDate, EndDate) values ( 1011, 9, '2013-11-08', Null);
+
+insert into employmenthistory (EmployeeID, PositionID, StartDate, EndDate) values ( 1012, 8, '2002-11-08', Null);
+
+insert into employmenthistory (EmployeeID, PositionID, StartDate, EndDate) values ( 1013, 7, '2008-11-08', Null);
+
+insert into employmenthistory (EmployeeID, PositionID, StartDate, EndDate) values ( 1014, 6, '1999-11-08', Null);
+
+insert into employmenthistory (EmployeeID, PositionID, StartDate, EndDate) values ( 1015, 5, '2013-11-08', Null);
+
+insert into employmenthistory (EmployeeID, PositionID, StartDate, EndDate) values ( 1016, 4, '2014-11-08', Null);
+
+insert into employmenthistory (EmployeeID, PositionID, StartDate, EndDate) values ( 1017, 3, '2007-11-08', Null);
+
+insert into employmenthistory (EmployeeID, PositionID, StartDate, EndDate) values ( 1018, 2, '2006-11-08', Null);
+
+insert into employmenthistory (EmployeeID, PositionID, StartDate, EndDate) values ( 1019, 1, '2009-11-08', Null);
+
+
+
+
